dc1b8e0f999ac74e53b57636fbe90683d059b1f0
Ali Alhashimi <libmacali@gmail.com>
2020-04-06 19:06:34 +0400

    added timer to refresh count

--- app/lib/screens/home_screen.dart
+++ app/lib/screens/home_screen.dart
@@ -1,9 +1,7 @@
-
 import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-
 
 class HomePage extends StatefulWidget {
   @override
@@ -12,80 +10,87 @@ class HomePage extends StatefulWidget {
 
 class HomePageState extends State<HomePage> {
   Timer interval;
-  final String url = "https://standforsudan.ebs-sd.com/StandForSudan/getStandForSudanStatstics";
+
+  final String url =
+      "https://standforsudan.ebs-sd.com/StandForSudan/getStandForSudanStatstics";
   var numberofDonations;
   var amount;
 
-@override
-void initState(){
-  super.initState();
-  
-  this.getJsonDate();
-}
+  @override
+  void initState() {
+    super.initState();
 
-Future<String> getJsonDate() async{
-  var response = await http.get(
-    //Url Encoding 
-    Uri.encodeFull(url),
-    //Accept only Json response
-    headers: {"Accept": "application/json"}
-  );
+    Timer.periodic(Duration(seconds: 60), (Timer t) {
+      this.getJsonDate();
+    });
+  }
 
-  print(response.body);
+  Future<String> getJsonDate() async {
+    var response = await http.get(
+        //Url Encoding
+        Uri.encodeFull(url),
+        //Accept only Json response
+        headers: {"Accept": "application/json"});
 
-  setState(() {
-    var parsedData = json.decode(response.body);
-    numberofDonations = parsedData['numberOfTransaction'];
-    amount = parsedData['totalAmount'];
-  });
+    //print(response.body);
 
-  return "Success";
-} 
+    setState(() {
+      var parsedData = json.decode(response.body);
+      numberofDonations = parsedData['numberOfTransaction'];
+      amount = parsedData['totalAmount'];
+    });
 
-@override
+    return "Success";
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Container(
           child: Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(top:30.0),
-                    child: Column(
-                      children: <Widget>[
-                        Text(
-                          "جملة التبرعات",
-                          style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          amount.toString(),
-                          style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
-                        )
-                      ],
-                    ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(top: 30.0),
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        "جملة التبرعات",
+                        style: TextStyle(
+                            fontSize: 25.0, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        amount.toString(),
+                        style: TextStyle(
+                            fontSize: 25.0, fontWeight: FontWeight.bold),
+                      )
+                    ],
                   ),
-                  SizedBox(
-                    height: 20.0,
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 30.0),
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        "عدد المتبرعين",
+                        style: TextStyle(
+                            fontSize: 25.0, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        numberofDonations.toString(),
+                        style: TextStyle(
+                            fontSize: 25.0, fontWeight: FontWeight.bold),
+                      )
+                    ],
                   ),
-                   Padding(
-                     padding: EdgeInsets.only(left:30.0),
-                     child: Column(
-                      children: <Widget>[
-                        Text(
-                          "عدد المتبرعين",
-                          style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          numberofDonations.toString(),
-                          style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
-                        )
-                      ],
-                  ),
-                   ),
-                ],
-              ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
