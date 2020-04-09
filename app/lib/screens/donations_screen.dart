9afe572ab49ea98a3d97007c42c1b61f84c53444
Ali Alhashimi <libmacali@gmail.com>
2020-04-09 21:37:41 +0400

    how to changes

--- app/lib/screens/donations_screen.dart
+++ app/lib/screens/donations_screen.dart
@@ -112,7 +112,7 @@ class _DonationsScreenState extends State<DonationsScreen> {
     return "Success";
   }
 
-  void onHowToButtonPressed(BuildContext context) => Navigator.push(context, MaterialPageRoute(builder: (context) => HowToPageWidget()));
+  void onHowToButtonPressed(BuildContext context) => Navigator.push(context, MaterialPageRoute(builder: (context) => HowToDonate()));
 
   @override
   Widget build(BuildContext context) {
@@ -121,7 +121,7 @@ class _DonationsScreenState extends State<DonationsScreen> {
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(MediaQuery.of(context).size.height / 4),
         child: AppBar(
-          flexibleSpace: HeaderWidget(),
+          flexibleSpace: HeaderWidget(title: 'الإحصائيات',),
           elevation: 0.0,
         ),
       ),
